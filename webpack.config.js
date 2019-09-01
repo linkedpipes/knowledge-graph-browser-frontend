689f46ab9b5c0e9b4f652103c6075ee990f87602
Štěpán Stenchlák <s.stenchlak@gmail.com>
2019-09-01 23:04:36 +0200

    Added Vue.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
 const path = require('path');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
 
 module.exports = {
   entry: './src/index.ts',
   devtool: 'inline-source-map',
   module: {
     rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      },
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
@@ -16,6 +21,9 @@ module.exports = {
   resolve: {
     extensions: [ '.tsx', '.ts', '.js' ]
   },
+  plugins: [
+    new VueLoaderPlugin()
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
