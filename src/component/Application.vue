14e671fbc55d86623bc3b5545ddeb8283fa30e7e
Štěpán Stenchlák <s.stenchlak@gmail.com>
2020-02-28 18:59:03 +0100

    Basic Application component structure update

--- src/component/Application.vue
+++ src/component/Application.vue
@@ -2,11 +2,9 @@
     <v-app class="app">
         <v-toolbar style="flex: none;">
             <v-toolbar-items>
-                <v-btn @click="$refs.addNode.show()" text>Add node(s)</v-btn> <!-- todo: or multiple nodes -->
-                <v-btn @click="showHidden" text>Load</v-btn>
-                <v-btn @click="showHidden" text>Save</v-btn>
-                <v-btn @click="showHidden" text>View all hidden nodes</v-btn>
-                <v-btn @click="expandAll" text>[Experimental] expand all</v-btn>
+                <v-btn @click="$refs.addNode.show()" text>{{ $t("new_nodes.button") }}</v-btn>
+                <v-btn text>{{ $t("load_dialog.button") }}</v-btn>
+                <v-btn @click="$refs.saveDialog.show()" text>{{ $t("save_dialog.button") }}</v-btn>
             </v-toolbar-items>
 
             <div class="flex-grow-1"></div>
@@ -25,7 +23,7 @@
                 <v-btn click="$refs.configurationStylesheetDialog.show()" text>{{ $t("change_configuration_and_stylesheet") }}</v-btn>
             </v-toolbar-items>
 
-            <template #extension> 
+            <template #extension>
                 <span class="mr-2" v-if="!filter_active">No vertices were filtered out.</span>
                 <span class="mr-2" v-if="filter_active">Filtered by:</span>
                 <span class="filter-list" v-if="filter_active">
@@ -38,135 +36,90 @@
             </template>
         </v-toolbar>
         <div class="d-flex flex-grow-1" style="overflow: hidden;">
-            <graph-area ref="graphArea" :application-data="data" />
-            <div style="overflow: auto;" :class="{'detail-panel-active': true}">
-                <detail-panel ref="detail"/>
-            </div>
+            <graph-area :graph="graph" />
+            <side-panel :graph="graph"/>
         </div>
-        <add-node ref="addNode" :application-data="data"></add-node>
 
+        <add-node ref="addNode" :graph="graph" />
+        <save-dialog ref="saveDialog" />
         <configuration-stylesheet-dialog
             ref="configurationStylesheetDialog"
             :oldConfiguration="configurationIRI"
             :oldStylesheet="stylesheetIRI"
             @changed="configurationStylesheetUpdated"
-        ></configuration-stylesheet-dialog>
+        />
     </v-app>
 </template>
 
 <script lang="ts">
-import GraphArea from './GraphArea.vue';
-import DetailPanel from './DetailPanel.vue';
+import GraphArea from './graph/GraphArea.vue';
 import AddNode from './AddNode.vue';
-import ConfigurationStylesheetDialog from './ConfigurationStylesheetDialog.vue'
+import ConfigurationStylesheetDialog from './ConfigurationStylesheetDialog.vue';
+import { DataGraphFetcher } from '../graph-fetcher/DataGraphFetcher';
 import { Graph } from '../graph/Graph';
+import SidePanel from './side-panel/SidePanel.vue';
+import SaveDialog from './SaveDialog.vue';
+
 export default {
     data: function() {
+        let fetcher = new DataGraphFetcher("http://localhost:3000/", "https://linked.opendata.cz/resource/knowledge-graph-browser/configuration/psp");
+        let graph = new Graph(fetcher);
+
+        graph.createNode("https://psp.opendata.cz/zdroj/osoba/5914").useDefaultView().then((view) => view.fetchPreview());
+        graph.createNode("https://psp.opendata.cz/zdroj/osoba/5915").useDefaultView().then((view) => view.fetchPreview());
+
         return {
             configurationIRI: null as String,
             stylesheetIRI: null as String,
-            test: Object,
+            remoteURL: "http://localhost:3000/",
+            graph: graph,
             filter_active: false,
+            truefalse: false
         };
     },
+    created: function () {
+        this.fetcher = new DataGraphFetcher(this.remoteURL, null);
+        this.translations = [];
+        for (let code in this.$root.$i18n.messages) {
+            this.translations.push({
+                text: this.$root.$i18n.messages[code]['_lang_local'],
+                value: code
+            });
+        }
+    },
+    watch: {
+        configurationIRI: function(IRI: string) {
+            //this.graph.edges = {};
+            //this.graph.nodes = {};
+            this.fetcher = new DataGraphFetcher(this.remoteURL, IRI);
+        },
+
+        stylesheetIRI: function(stylesheet: string) {
+            this.stylesheetData = {};
+            //this.fetcher.getStylesheet(stylesheet).then(ss => this.data.graph.CyInstance.style(ss.styles.map(style => { return {selector: style.selector, css: style.properties}})));
+        }
+    },
     components: {
-        DetailPanel,
         GraphArea,
+
+        // Component responsible for window where user can add new nodes
         AddNode,
-        ConfigurationStylesheetDialog
+
+        // Right-side pannel for detail about nodes
+        SidePanel,
+        ConfigurationStylesheetDialog,
+        SaveDialog,
     },
     mounted: function() {
         this.$refs.configurationStylesheetDialog.show();
-/*         this.$refs.graphArea.mount(this.data.graph);
-        (<Graph>this.data.graph).CyInstance.on("tap", "node", async (evt) => {
-            let node: Node = evt.target.scratch("_node");
-            console.log("Clicked on node", node, this.$refs);
-            this.$refs.detail.mountNode(node);
-        });
- */    },
-    created: function() {
-        this.data = {
-            graph: null as Graph
-        }
+    },
 
-
- /*        // Set new non-reactive property
-        this.data.graph = new Graph();
-
-        let graph: Graph = this.data.graph;
-
-        graph.fetchNode("http://www.wikidata.org/entity/Q7377").then(n => n.show());
-
-        //let ss = await this.fetcher.getStylesheet("https://linked.opendata.cz/resource/knowledge-graph-browser/rpp/style-sheet");
-        graph.fetcher.getStylesheet("https://linked.opendata.cz/resource/knowledge-graph-browser/wikidata/animals/style-sheet")
-            .then(ss => this.data.graph.CyInstance.style(ss.styles.map(style => { return {selector: style.selector, css: style.properties}})));
- */    },
     methods: {
         configurationStylesheetUpdated: function(update: {configuration: string, stylesheet: string, predefinedStartedNodeIRI: string}) {
-            let confChanged = update.configuration != this.configurationIRI;
-
-            // First chceck if configuration was updated
-            if (confChanged) {
-                if (this.configurationIRI) {
-                    // Need to properly unmount
-                    this.$refs.graphArea.unmount();
-                    this.$refs.detail.unmount();
-                }
-
-                // Set new non-reactive property
-                this.data.graph = new Graph(update.configuration);
-                this.configurationIRI = update.configuration;
-
-                // Mount graph instance
-                this.$refs.graphArea.mount();
-            }
-
-            // Update stylesheet
-            if (confChanged || update.stylesheet != this.stylesheetIRI) {
-                (<Graph>this.data.graph).fetcher.getStylesheet(update.stylesheet)
-                .then(ss => this.data.graph.CyInstance.style(ss.styles.map(style => { return {selector: style.selector, css: style.properties}})));
-
-                this.stylesheetIRI = update.stylesheet;
-            }
-
-            // Mounting
-            if (confChanged) {
-                (<Graph>this.data.graph).CyInstance.on("tap", "node", async (evt) => {
-                    let node: Node = evt.target.scratch("_node");
-                    this.$refs.detail.mountNode(node);
-                });
-
-                // Show add node dialog
-                this.$refs.addNode.show(update.predefinedStartedNodeIRI);
-            }
+            this.configurationIRI = update.configuration;
+            this.stylesheetIRI = update.stylesheet;
         },
-        expandAll: function() {
-            let graph: Graph = this.data.graph;
-
-            graph.getAllNodes().forEach(node => {
-                node.getViewSets().then(() => {
-                    for (let vs in node.viewSets) {
-                        for (let v in node.viewSets[vs].views) {
-                            node.viewSets[vs].views[v].use();
-                            node.viewSets[vs].views[v].expand().then(expansion => {
-                                expansion.nodes.forEach(n => {n.cyInstance.removeStyle("display")});
-                                expansion.edges.forEach(n => {n.cyInstance.removeStyle("display")});
-                                graph.CyInstance.layout({name: 'grid'}).run();
-                            });
-                        }
-                    }
-                });
-            });
-        },
-        showHidden: function() {
-            let graph: Graph = this.data.graph;
-            
-            for(let node of graph.getAllNodes()) {
-                node.userHidden = false;
-            }
-        }
     }
-
 }
 </script>
 
@@ -181,9 +134,5 @@ html {
     bottom: 0;
     left: 0;
     right: 0;
-}
-
-.detail-panel-active {
-    width: 30%;
 }
 </style>
