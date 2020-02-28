399309204c04f73f3bad151c0374548657cb741c
Štěpán Stenchlák <s.stenchlak@gmail.com>
2020-02-28 18:48:39 +0100

    Code cleanup

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,11 +3,17 @@ import VueI18n from 'vue-i18n';
 
 Vue.use(VueI18n);
 
+// This loads all the /locales files
+let localesContext = require.context("../locales", true, /\.yaml$/);
+let locales: VueI18n.LocaleMessages = {};
+localesContext.keys().forEach(function(key: string){
+    let code = (key.split('\\').pop().split('/').pop().split('.'))[0];
+    locales[code] = <VueI18n.LocaleMessageObject>localesContext(key);
+});
+
+// Configure i18n
 export default new VueI18n({
-  locale: 'en',
-  fallbackLocale: 'en',
-  messages: {
-    en: require("../locales/en.yaml"),
-    cs: require("../locales/cs.yaml")
-  }
+    locale: 'en',
+    fallbackLocale: 'en',
+    messages: locales
 });
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,9 @@
 import Vue from 'vue';
 import Application from './component/Application.vue';
 import Vuetify from 'vuetify/lib';
-import VueI18n from 'vue-i18n';
+import i18n from './i18n';
 
-Vue.use(VueI18n)
 Vue.use(Vuetify);
-
-// This loads all the /locales files
-let localesContext = require.context("../locales", true, /\.yaml$/);
-let locales: VueI18n.LocaleMessages = {};
-localesContext.keys().forEach(function(key: string){
-    let code = (key.split('\\').pop().split('/').pop().split('.'))[0];
-    locales[code] = <VueI18n.LocaleMessageObject>localesContext(key);
-});
-
-// Configure i18n
-const i18n = new VueI18n({
-    locale: 'en',
-    fallbackLocale: 'en',
-    messages: locales
-});
 
 const vuetify = new Vuetify({
     icons: {
