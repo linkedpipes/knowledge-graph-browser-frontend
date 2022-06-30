ad75a5b4da72c3ab6b54d49e28da54d94af678d0
Jiří Resler <j.resler.29@gmail.com>
2022-07-01 00:56:14 +0200

    added DFS

--- src/component/facet-filtering/DynamicallyGeneratedFacets.vue
+++ src/component/facet-filtering/DynamicallyGeneratedFacets.vue
@@ -16,6 +16,15 @@ export default class DynamicallyGeneratedFacets extends Vue {
     this.addEdgesToNodes();
 
     console.log(this.graph);
+
+    console.log(this.facets);
+
+    let dynamicFacets = new Set();
+
+    const testNode = this.graph.nodes["http://www.wikidata.org/entity/Q937"];
+
+    this.findFacets(dynamicFacets, testNode);
+    console.log(dynamicFacets)
   }
 
   addEdgesToNodes() {
@@ -37,6 +46,59 @@ export default class DynamicallyGeneratedFacets extends Vue {
         type: edge.type
       });
     }
+  }
+
+  findFacets(dynamicFacets, initialNode) {
+    // Implement DFS
+    let stack = [];
+
+    const maxDepth = 2;
+
+    stack.push({
+      node: initialNode,
+      depth: 0,
+      path: "",
+      parentNode: null
+    });
+
+    while (stack.length > 0) {
+      const currentNodeInfo = stack.pop();
+
+      //  Do not look for facets further from initialNode
+      //  than the specified depth
+      if (currentNodeInfo.depth > maxDepth) {
+        continue;
+      }
+
+      // Insert node's neighbours to the stack
+      for (const edge of currentNodeInfo.node.connectedEdges) {
+        // Skip the "parent" node
+        if (edge.otherNode == currentNodeInfo.parentNode) {
+          continue;
+        }
+
+        stack.push({
+          node: edge.otherNode,
+          depth: currentNodeInfo.depth + 1,
+          // cesta by mala obsahovať aj šípky
+          path: currentNodeInfo.path + (currentNodeInfo.path == "" ? "" : "/") + edge.type.label,
+          parentNode: currentNodeInfo.node
+        })
+      }
+
+      // Process the current node
+      // Skip the initial node
+      if (currentNodeInfo.depth == 0) {
+        continue
+      }
+
+      dynamicFacets.add({name: currentNodeInfo.node.currentView.preview.label, path: currentNodeInfo.path});
+
+
+
+
+    }
+
   }
 }
 </script>
